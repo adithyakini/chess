1584cf6f9da36e9213be000d9989e8d68d61d3f8
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-06-09 10:16:16 +0530

    Update board.py

--- chess_engine/board.py
+++ chess_engine/board.py
@@ -1,1 +1,23 @@
+import chess
 
+def initialize_board():
+
+    return chess.Board()
+
+
+def make_move(board, move_uci):
+
+    try:
+
+        move = chess.Move.from_uci(move_uci)
+
+        if move in board.legal_moves:
+
+            board.push(move)
+
+            return True
+
+    except:
+        pass
+
+    return False
