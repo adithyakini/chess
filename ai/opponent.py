960588827d508cde75969187f6e0d0401ea4cb1a
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-06-09 11:50:40 +0530

    Update opponent.py

--- ai/opponent.py
+++ ai/opponent.py
@@ -1,10 +1,15 @@
 import chess.engine
+import streamlit as st
 
-engine = chess.engine.SimpleEngine.popen_uci(
-    "stockfish"
-)
+@st.cache_resource
+def get_engine():
+    return chess.engine.SimpleEngine.popen_uci(
+        "stockfish"
+    )
 
 def get_ai_move(board):
+
+    engine = get_engine()
 
     result = engine.play(
         board,
